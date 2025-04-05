97e8e2ca25d6a04f8a62278672d6344010587958
prathammittall <prathamv0106@gmail.com>
2025-04-05 22:58:42 +0530

    Updated Routes in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 import { BrowserRouter } from 'react-router-dom'
 import Navbar from './pages/components/Home/Navbar'
 import Hero from './pages/components/Home/Hero'
-import Events from './pages/components/Home/Events'
-import Contact from './pages/components/Home/Contact'
-import Footer from './pages/components/Home/Footer'
+// import Events from './pages/components/Home/Events'
+// import Contact from './pages/components/Home/Contact'
+// import Footer from './pages/components/Home/Footer'
 
 function App() {
     return (
