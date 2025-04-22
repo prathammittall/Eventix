c592ee37e6cee2cfe904d13b2dbdf95fb89cdff2
Vanshita Taneja <vanshita0156.becse24@chitkara.edu.in>
2025-04-22 13:49:09 +0530

    Made event section

--- src/pages/components/Home/Events.jsx
+++ src/pages/components/Home/Events.jsx
@@ -3,7 +3,7 @@ import Cards from './Cards'
 import { motion } from 'framer-motion';
 
 function Events() {
-  // State for search input and filter selection
+  // State for search input and filter selections
   const [searchQuery, setSearchQuery] = useState('');
   const [selectedFilter, setSelectedFilter] = useState('All');
   const [displayCount, setDisplayCount] = useState(8); // Initially show 8 events
