21122af894d304df7f880cd75bcc06ebe7d553aa
harshita <harshita1230.becse24@chitkara.edu.in>
2025-04-27 09:23:25 +0530

    Enhance hover states and subtle animations for footer elements

--- src/pages/components/Home/Footer.jsx
+++ src/pages/components/Home/Footer.jsx
@@ -5,6 +5,7 @@ import { useState } from "react";
 const Footer = () => {
     const [email, setEmail] = useState('');
 
+
     // Animation variant
     const containerVariants = {
         hidden: { opacity: 0 },
