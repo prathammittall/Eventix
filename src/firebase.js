a8d7c12c3f568599e0e606d44928d5790189a4c2
Prathammittall <prathamv0106@gmail.com>
2025-05-06 11:14:00 +0530

    Firebase initiated

---
+++ src/firebase.js
@@ -0,0 +1,26 @@
+import { initializeApp } from 'firebase/app';
+import { getAuth, GoogleAuthProvider, GithubAuthProvider } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBsijI7_38slaqDonIPHc4kAICRrjeEJe4",
+    authDomain: "eventix-35718.firebaseapp.com",
+    projectId: "eventix-35718",
+    storageBucket: "eventix-35718.firebasestorage.app",
+    messagingSenderId: "905213185218",
+    appId: "1:905213185218:web:a82183070665fe03c23f5b",
+    measurementId: "G-HZKYPR7ELF"
+};
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const db = getFirestore(app);
+
+// Configure Google Provider with profile access
+export const googleProvider = new GoogleAuthProvider();
+googleProvider.addScope('profile');
+googleProvider.addScope('email');
+
+// Configure GitHub Provider
+export const githubProvider = new GithubAuthProvider();
+export default app;
